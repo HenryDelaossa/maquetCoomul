b7898a389b308d1de7050e58fe920e0bad272e04
HenryDelaossa <delaossah@gmail.com>
2022-06-26 13:50:26 -0500

    ..

--- js/apiGetData.js
+++ js/apiGetData.js
@@ -6,7 +6,7 @@
  */
 const getData = async (enlace) => {
     const res = await fetch(enlace);
-    if (res) {
+    if (res.status == 200) {
         return res.json()
     } else {
         console.log(res.status)
